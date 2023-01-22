3af26bd34cb07bb1d8548f05b1e59c2c286ee632
Ariq Daffa <ariqdaffaathallah@gmail.com>
2023-01-23 02:20:24 +0700

    feat: group cases by cities and get lat long for each city

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
+import { json } from 'd3';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +10,8 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
   constructor(private http: HttpClient) {}
+
+  private cities: Object;
 
   ngOnInit(): void {
     let map: L.Map;
@@ -107,29 +110,50 @@ export class MapComponent implements OnInit {
         click: zoomToFeature,
       });
     }
+    // this.getCities('Kota Bandung')
+    // console.log(this.getCities('Kota Bandung'));
 
-    this.http.get('https://opendata.jabarprov.go.id/api-backend/bigdata/dinkes/od_18510_jumlah_kasus_hiv_berdasarkan_kabupatenkota').subscribe((mapResponse: any) => {
-      let mapArray = [];
+    this.http
+      .get(
+        'https://opendata.jabarprov.go.id/api-backend/bigdata/dinkes/od_18510_jumlah_kasus_hiv_berdasarkan_kabupatenkota'
+      )
+      .subscribe((mapResponse: any) => {
+        let mapArray = [];
 
-      const result = mapResponse.data.reduce(function (
-        r: any,
-        a: any
-      ) {
-        r[a.nama_kabupaten_kota] = r[a.nama_kabupaten_kota] || [];
-        r[a.nama_kabupaten_kota].push({
-          'jumlah_kasus': a.jumlah_kasus,
-          'tahun': a.tahun
+        const result = mapResponse.data.reduce(function (r: any, a: any) {
+          r[a.nama_kabupaten_kota] = r[a.nama_kabupaten_kota] || [];
+          r[a.nama_kabupaten_kota].push({
+            jumlah_kasus: a.jumlah_kasus,
+            tahun: a.tahun,
+          });
+          return r;
+        }, []);
+
+        Object.keys(result).map((key) => {
+          const sum = result[key].reduce(
+            (total: any, item: any) => total + parseInt(item.jumlah_kasus),
+            0
+          );
+
+          this.http.get('assets/cities.json').subscribe((jsonResponse: any) => {
+            const response = jsonResponse.cities.find(
+              (element) => element.name === key
+            );
+            mapArray.push({
+              KabupatenKota: key,
+              JumlahKasus: sum,
+              latitude: response.latitude,
+              longitude: response.longitude,
+            });
+          });
         });
-        return r;
-      }, []);
 
-      Object.keys(result).map((key) => {
-        const sum = result[key].reduce((total: any, item: any) => total + parseInt(item.jumlah_kasus), 0);
-        mapArray.push({
-          KabupatenKota: key,
-          JumlahKasus: sum
-        })
-      })
-    });
+        console.log(mapArray);
+
+        // geojson = L.geoJson(statesData, {
+        //   style: style,
+        //   onEachFeature: onEachFeature,
+        // }).addTo(map);
+      });
   }
 }
